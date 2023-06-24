b2efac724c9508c2d4bed77df1dc8a755382fe09
Phat Nguyen <manhphat1157@gmail.com>
2023-06-24 01:17:23 -0400

    first commit

---
+++ .history/Product/app/config/db.config_20230620011716.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://blessjiboye1234:D2dBw7IeU9zhGRIj@cluster0.ysd0npa.mongodb.net/SportStore?retryWrites=true&w=majority"
+};
---
+++ Product/app/config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://manhphat1157:ksxCFe7bnwcqzjtN@cluster0.kon7m0d.mongodb.net/SportStore?retryWrites=true&w=majority"
+};
